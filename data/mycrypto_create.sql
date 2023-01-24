836f5b4d731a3a988bf79657ed7b29f57dc55809
inmiti <inmabauvel@gmail.com>
2023-01-24 22:57:33 +0100

    Creada estructura MVC, creado base.html

---
+++ data/mycrypto_create.sql
@@ -0,0 +1,10 @@
+ CREATE TABLE "mycrypto" (
+	"id"	INTEGER,
+	"date"	TEXT,
+	"time"	TEXT,
+	"moneda_from"	TEXT,
+	"cantidad_from"	NUMERIC,
+	"moneda_to"	TEXT,
+	"cantidad_to"	NUMERIC,
+	PRIMARY KEY("id" AUTOINCREMENT)
+);
---
+++ readme.md
@@ -0,0 +1,7 @@
+## Instalar entorno
+Instalar entorno usando requirements.txt
+Cambiar nombre a .env y añadir 
+## Base de datos
+-- Crear base de datos mycrypto_create.sql en el directorio data. 
+
+Config
