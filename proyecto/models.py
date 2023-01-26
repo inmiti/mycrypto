55dd0351a4ca2cff86a540c2126435b7294e8c1f
inmiti <inmabauvel@gmail.com>
2023-01-26 23:51:54 +0100

    index con base de datos sqlite, funcion select all en model

---
+++ proyecto/models.py
@@ -0,0 +1,21 @@
+import sqlite3
+from config import *
+
+def select_all():
+    con = sqlite3.connect(ORIGIN_DATA)
+    cur = con.cursor()
+    res = cur.execute("SELECT date, time, moneda_from, cantidad_from, moneda_to, cantidad_to FROM mycrypto order by date")
+    filas = res.fetchall()
+    columnas= res.description
+
+    resultado = []
+
+    for fila in filas:
+        dato = {}
+        posicion_col = 0
+        for campo in columnas:
+            dato[campo[0]]=fila[posicion_col]
+            posicion_col+=1
+        resultado.append(dato)
+
+    return resultado
--- proyecto/routes.py
+++ proyecto/routes.py
@@ -1,9 +1,20 @@
 from proyecto import app
 from flask import render_template
+from proyecto.models import select_all
 
 @app.route("/")
 def index():
-    return render_template("index.html", pageTitle="Movimientos")
+    registros = select_all()
+    return render_template("index.html", pageTitle="Movimientos", data=registros)
+
+'''
+    data_tabla=[
+    {'id':1, 'date':'2023-01-15','time':'14:16:01', 'moneda_from':'EUR', 'cantidad_from':3000.00, 'moneda_to':'BTC', 'cantidad_to':1.40},
+    {'id':2, 'date':'2023-01-19','time':'13:46:21', 'moneda_from':'BTC', 'cantidad_from':1.00, 'moneda_to':'ECH', 'cantidad_to':50.78},
+    {'id':3, 'date':'2023-01-15','time':'14:16:01', 'moneda_from':'BTC', 'cantidad_from':0.40, 'moneda_to':'EUR', 'cantidad_to':2500.00}
+]
+'''
+    
 
 @app.route("/purchase")
 def pursache():
