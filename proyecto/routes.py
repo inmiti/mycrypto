02a65faebfb2c01245c3161a9a07c12595461626
inmiti <inmabauvel@gmail.com>
2023-01-28 17:54:03 +0100

    hecho post con request.form y validacion de datos

--- proyecto/routes.py
+++ proyecto/routes.py
@@ -5,34 +5,34 @@ from datetime import date
 
 
 def validateForm(requestForm):
-    #hoy = date.today().isoformat()
     errores=[]
     if requestForm['moneda_from'] == "" or requestForm['moneda_to'] == "" :
         errores.append("tipo de moneda vacio: Seleccione una moneda de la lista")
-    if requestForm['cantidad_from'] == "" or requestForm['cantidad_to'] == "":
-        errores.append("cantidad vacia: Introduzca una cantidad positiva")   
-    if float(requestForm['cantidad_from']) <= 0.0 or float(requestForm['cantidad_to']) <= 0.0:
+    #if requestForm['cantidad_from'] == "" or requestForm['cantidad_to'] == "":
+        #errores.append("cantidad vacia: Introduzca una cantidad positiva")   
+    if requestForm['cantidad_from'] <= "" or float(requestForm['cantidad_from']) <= 0.0 or requestForm['cantidad_to'] <= "" or float(requestForm['cantidad_to']) <= 0.0:
         errores.append("cantidad negativa o nula: Introduzca una cantidad positiva")   
+    
     return errores
 
 @app.route("/")
-def index():
+def inicio():
 
     registros = select_all()
     return render_template("index.html", pageTitle="Movimientos", data=registros)
     
 
 @app.route("/purchase",methods=["GET","POST"])
-def purchase():
+def operar():
     if request.method == "GET":
         return render_template("purchase.html", pageTitle="Compras_Ventas_Intercambios")
     else:
-        print("esto recibo: ", request.form)
-        '''
         errores = validateForm(request.form)
         if errores:
             return render_template("purchase.html", pageTitle="Compras_Ventas_Intercambios", msgError=errores)
         else:
             return render_template("purchase.html", pageTitle="Compras_Ventas_Intercambios")
-        '''
+       
+        
+        
         
